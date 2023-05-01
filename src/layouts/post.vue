10b282b4e55f66abaf6831bd77ac5f6ef76355ba
Don <Don@MacBook-Air.fritz.box>
2023-05-01 23:19:29 +0200

    Footer + CTA

--- src/layouts/post.vue
+++ src/layouts/post.vue
@@ -18,5 +18,15 @@
         </Transition>
       </template>
     </RouterView>
+     <CTA
+      header_1="Sie möchten einen Beitrag einreichen?"
+      header_2="Schreiben Sie uns gerne Ihre Ideen"
+      body="Sowohl unser Blog als auch unsere Zeitschrift 'Humanität' freut sich über kreative Ideen innerhalb der Freimaurerei."
+      primary_btn_text="Jetzt Ideen einreichen"
+      secondary_btn_text="Mehr erfahren"
+      primary_btn_link=""
+      secondary_btn_link=""
+      />
+    <Footer />
   </div>
 </template>
